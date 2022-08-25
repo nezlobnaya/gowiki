b14cede9b1aa882201d084cec65a0b320542b5b5
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2022-08-25 13:12:00 -0400

    First commit.messing with templates in Go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nezlobnaya/gowiki
+
+go 1.19
---
+++ wiki.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// Let's start by defining the data structures.
+type Page struct {
+	Title string
+	Body  []byte //a byte slice The Body element is a []byte rather than string because that is the type expected by the io libraries we will use
+}
+
+// persistent storage? We can address that by creating a save method on Page:
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+// The function loadPage constructs the file name from the title parameter,
+// reads the file's contents into a new variable body,
+// and returns a pointer to a Page literal constructed with the proper title and body values.
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+// The function viewPage is a simple function that prints the title and body of the Page it is passed.
+func viewPage(p *Page) {
+	fmt.Println(p.Title)
+	fmt.Println(string(p.Body))
+}
+
+func main() {
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	viewPage(p2)
+}
